ba87654cd137603b89f688c9bc905b5572db5f68
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-07 21:48:28 +0500

    code is added

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const money_plus = document.getElementById('money_plus');
 const money_minus = document.getElementById('money_minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+const btn = document.getElementById('btn');
