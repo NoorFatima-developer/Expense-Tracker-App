4a35f586f900c714c536c2cc7fdcdddf23c498a4
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-07 22:09:44 +0500

    code is added

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ function addTransactionDOM(transaction){
     item.classList.add(transaction.amount < 0 ? "minus" : "plus");
     item.innerHTML = `
     $(transaction.text)<span>${sign}${Math.abs(transaction.amount)}</span>
-    
+    <button class = "delete-btn" onclick = "">X</button>
     `
 }
